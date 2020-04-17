ae3f8522e4be319348d64df9d1018a1b72d1fbc9
Scrounger <scrounger@gmx.net>
2020-04-17 21:31:20 +0200

    main functions aded

--- main.js
+++ main.js
@@ -12,6 +12,15 @@ const utils = require('@iobroker/adapter-core');
 // const fs = require("fs");
 
 let connected = null;
+let SQLFuncs = null;
+let usedDatapoints = null;
+
+const dbNames = [
+	'ts_number',
+	'ts_counter',
+	'ts_string',
+	'ts_bool'
+];
 
 class Sqlstatistics extends utils.Adapter {
 
@@ -44,6 +53,8 @@ class Sqlstatistics extends utils.Adapter {
 			// Check connection to instance
 			connected = await this.checkConnection();
 
+			//TODO: interval einfügen
+
 			this.updateStatistic();
 		}
 	}
@@ -56,8 +67,74 @@ class Sqlstatistics extends utils.Adapter {
 				if (instanceObj && instanceObj.native) {
 					if (instanceObj.native.dbtype !== 'sqlite') {
 						this.log.info(`SQL History Statistic connected with '${this.config.sqlInstance}'. Updating statistics...`);
-						
 
+						usedDatapoints = [];
+						let updateStart = new Date().getTime();
+
+						SQLFuncs = await require(__dirname + '/lib/' + instanceObj.native.dbtype);
+
+						let databaseList = await this.getQueryResult(SQLFuncs.getDatabases());
+
+						if (databaseList && Object.keys(databaseList).length > 0) {
+							let totalSize = 0;
+							let totalRows = 0;
+
+							for (const database of databaseList) {
+								if (!database.name.includes("_schema")) {
+									let idDatabasePrefix = `databases.${database.name}`;
+									let databaseRows = 0;
+
+									// store database statistics
+									totalSize = totalSize + database.size;
+									await this.createStatisticObjectNumber(`${idDatabasePrefix}.size`, "size of database", 'MB');
+									this.setMyState(`${idDatabasePrefix}.size`, database.size, true);
+
+									// table statistics
+									let databaseTableList = await this.getQueryResult(SQLFuncs.getTablesOfDatabases(database.name));
+									if (databaseTableList && Object.keys(databaseTableList).length > 0) {
+										for (const table of databaseTableList) {
+											let idTablePrefix = `${idDatabasePrefix}.${table.name}`;
+
+											await this.createStatisticObjectNumber(`${idTablePrefix}.size`, 'size of table', 'MB');
+											this.setMyState(`${idTablePrefix}.size`, table.size, true);
+
+											let rowsCount = await this.getQueryResult(SQLFuncs.getRowsCountOfTable(database.name, table.name));
+											databaseRows = databaseRows + rowsCount[0].rows;
+
+											await this.createStatisticObjectNumber(`${idTablePrefix}.rows`, "rows in table", '');
+											this.setMyState(`${idTablePrefix}.rows`, rowsCount[0].rows, true);
+
+											if (database.name === instanceObj.native.dbname) {
+												await this.createIobSpecialTableStatistic(table, idTablePrefix, instanceObj);
+											}
+										}
+									}
+
+									await this.createStatisticObjectNumber(`${idDatabasePrefix}.rows`, "rows in database", '');
+									this.setMyState(`${idDatabasePrefix}.rows`, databaseRows, true);
+
+									totalRows = totalRows + databaseRows;
+								}
+							}
+
+							// store total sql statistics
+							await this.createStatisticObjectNumber(`total.size`, "total size of all databases", 'MB');
+							this.setMyState(`total.size`, totalSize, true);
+
+							await this.createStatisticObjectNumber(`total.rows`, "total rows of all databases", '');
+							this.setMyState(`total.rows`, totalRows, true);
+
+							// TODO alte DPs löschen
+							this.log.info(usedDatapoints.join(","));
+
+							let updateEnd = new Date().getTime();
+							let duration = Math.round(((updateEnd - updateStart) / 1000) * 100) / 100;
+
+							this.setState(`lastUpdate`, updateEnd, true);
+							this.setState(`lastUpdateDuration`, duration, true);
+
+							this.log.info(`Successful updating statistics in ${duration}s! `);
+						}
 					} else {
 						this.log.warn(`Database type 'SQLite3' is not supported!`);
 					}
@@ -72,6 +149,76 @@ class Sqlstatistics extends utils.Adapter {
 		}
 	}
 
+
+	/**
+	 * @param {{ name: string; size: number}} table
+	 * @param {string} idTablePrefix
+	 * @param {ioBroker.Object} instanceObj
+	 */
+	async createIobSpecialTableStatistic(table, idTablePrefix, instanceObj) {
+		let brokenRows = 0;
+		let brokenRowsList = [];
+
+		if (table.name === 'datapoints') {
+			// table datapoints -> check if Ids exist in Iob
+			let tableDatapoints = await this.getQueryResult(SQLFuncs.getRowsFromIobTableDatapoints(instanceObj.native.dbname));
+
+			if (tableDatapoints && Object.keys(tableDatapoints).length > 0) {
+				for (const row of tableDatapoints) {
+
+					let iobObj = await this.getForeignObjectAsync(row.name);
+					if (!iobObj) {
+						// Object not exist in IoB -> row is broken
+						brokenRows++;
+						brokenRowsList.push({ id: row.id, name: row.name, existInIoBroker: false });
+					} else if (iobObj && iobObj.common) {
+						if (!iobObj.common.custom || (iobObj.common.custom && !iobObj.common.custom[this.config.sqlInstance])) {
+							// Object exist in IoB but have no custom property for the current instance -> row is broken
+							brokenRows++;
+							brokenRowsList.push({ id: row.id, name: row.name, existInIoBroker: true });
+						}
+					}
+				}
+			}
+		} else {
+			// ohter iob tables
+			let tableDatapoints = await this.getQueryResult(SQLFuncs.getRowsFromIobTables(instanceObj.native.dbname, table.name));
+
+			if (tableDatapoints && Object.keys(tableDatapoints).length > 0) {
+				for (const row of tableDatapoints) {
+					if (dbNames.includes(table.name)) {
+						if (row.dead === 1) {
+							brokenRows = brokenRows + row.count;
+							brokenRowsList.push({ id: row.id });
+						}
+					}
+				}
+			}
+		}
+
+		if (dbNames.includes(table.name) || table.name === 'datapoints') {
+			await this.createStatisticObjectNumber(`${idTablePrefix}.brokenRows`, 'broken rows in table', '');
+			this.setMyState(`${idTablePrefix}.brokenRows`, brokenRows, true);
+
+			await this.createStatisticObjectString(`${idTablePrefix}.brokenRowsIds`, "ids of broken rows in table");
+			if (brokenRowsList.length > 0) {
+				this.setMyState(`${idTablePrefix}.brokenRowsIds`, JSON.stringify(brokenRowsList), true);
+			} else {
+				this.setMyState(`${idTablePrefix}.brokenRowsIds`, 'none', true);
+			}
+		}
+	}
+
+	/**
+	 * @param {string} id
+	 * @param {any} value
+	 * @param {boolean} ack
+	 */
+	setMyState(id, value, ack) {
+		this.setState(id, value, ack);
+		usedDatapoints.push(id);
+	}
+
 	async checkConnection() {
 		// check connection to sql instance on load
 		let instanceIsConnected = await this.getForeignStateAsync(`${this.config.sqlInstance}.info.connection`);
@@ -82,6 +229,65 @@ class Sqlstatistics extends utils.Adapter {
 			this.setState('info.connection', false, true);
 			return false;
 		}
+	}
+
+	/**
+	 * @param {string} query
+	 */
+	async getQueryResult(query) {
+		return new Promise((resolve, reject) => {
+			this.sendTo(this.config.sqlInstance, 'query', query, function (result) {
+				if (result && !result.error) {
+					resolve(result.result);
+				} else {
+					resolve(null);
+				}
+			});
+		});
+	}
+
+	/**
+	 * @param {string} id
+	 * @param {string} name
+	 * @param {any} unit
+	 */
+	async createStatisticObjectNumber(id, name, unit) {
+		let adapter = this;
+		this.setObjectNotExists(id, {
+			type: 'state',
+			common: {
+				name: name,
+				desc: 'sql statistic',
+				type: 'number',
+				unit: unit,
+				read: true,
+				write: false
+			},
+			native: {}
+		}, function (err, obj) {
+			if (!err && obj) adapter.log.debug('[updateStatistic] statistic object ' + id + ' created');
+		});
+	}
+
+	/**
+	 * @param {string} id
+	 * @param {string} name
+	 */
+	async createStatisticObjectString(id, name) {
+		let adapter = this;
+		this.setObjectNotExists(id, {
+			type: 'state',
+			common: {
+				name: name,
+				desc: 'sql statistic',
+				type: 'string',
+				read: true,
+				write: false
+			},
+			native: {}
+		}, function (err, obj) {
+			if (!err && obj) adapter.log.debug('[updateStatistic] statistic object ' + id + ' created');
+		});
 	}
 
 	/**
