faf49dc8f3021d082ad83f40da310b0613dc6174
Scrounger <scrounger@gmx.net>
2020-04-17 10:20:35 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,53 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'sqlstatistics adapter settings': {
+		'en': 'Adapter settings for sqlstatistics',
+		'de': 'Adaptereinstellungen für sqlstatistics',
+		'ru': 'Настройки адаптера для sqlstatistics',
+		'pt': 'Configurações do adaptador para sqlstatistics',
+		'nl': 'Adapterinstellingen voor sqlstatistics',
+		'fr': "Paramètres d'adaptateur pour sqlstatistics",
+		'it': "Impostazioni dell'adattatore per sqlstatistics",
+		'es': 'Ajustes del adaptador para sqlstatistics',
+		'pl': 'Ustawienia adaptera dla sqlstatistics',
+		'zh-cn': 'sqlstatistics的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	},
+	"Select sql history instance:": {
+		"en": "Select sql history instance:",
+		"de": "Wählen Sie die SQL History Instanz aus:",
+		"ru": "Выберите экземпляр истории sql:",
+		"pt": "Selecione a instância do histórico sql:",
+		"nl": "Selecteer sql history instance:",
+		"fr": "Sélectionnez l'instance d'historique sql:",
+		"it": "Seleziona l'istanza della cronologia sql:",
+		"es": "Seleccione la instancia del historial de SQL:",
+		"pl": "Wybierz wystąpienie historii sql:",
+		"zh-cn": "选择SQL历史记录实例："
+	}
+};
